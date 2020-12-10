db304e07974658651f3c75706fd452c4697ffee7
zhangsan <zhangsan@163.com>
2020-12-10 10:37:45 +0800

    解决冲突

--- git_demo/src/com/sxt/Test.java
+++ git_demo/src/com/sxt/Test.java
@@ -6,6 +6,8 @@ public class Test {
         System.out.println("111111111111111111111111111");
         System.out.println("222222222222222222222222");
         System.out.println("444444444444444");
+        System.out.println("3333333333333333333333333333333");
+        System.out.println("5555555555555555");
         System.out.println("666666666666666666666");
         System.out.println("3333333333333333333333333333333");
         System.out.println("5555555555555555");
